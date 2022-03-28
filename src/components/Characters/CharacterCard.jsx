f678d3838342b62a5da2e2ef1e71ae00958fd2e3
prophet7821 <samridhadas782001@gmail.com>
2022-03-28 15:59:16 +0530

    Final Update:Modal Responsive

--- src/components/Characters/CharacterCard.jsx
+++ src/components/Characters/CharacterCard.jsx
@@ -7,10 +7,16 @@ import {
   CardActionArea,
   CardMedia,
   Modal,
-  Box,
   Grid,
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+  Typography,
 } from "@mui/material";
 import { Text, Heading } from "@chakra-ui/react";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 const CharacterCard = (character) => {
   //states
@@ -25,9 +31,16 @@ const CharacterCard = (character) => {
     bgcolor: "black",
     color: "white",
     border: "2px solid #000",
-    "border-radius":"10px",
+    "border-radius": "10px",
     boxShadow: 24,
-    p:4
+    p: 1,
+    fontSize:{
+      xs:"0.4rem",
+      sm:"0.6rem",
+      md:"0.8rem",
+      lg:"1rem",
+      xl:"1.5rem"
+    }
   };
 
   return (
@@ -58,47 +71,119 @@ const CharacterCard = (character) => {
         }}
       >
         <Fade in={isOpen}>
-        <Grid container sx={styles} lg={8} md={8} sm={8} xs={8}justifyContent="center">
-        <Grid item mr={2} lg={4} md={8} sm={12} xs={12}>
-        <CardMedia
-            component="img"
-            alt={character.name}
-            image={character.image}
-          />
-        </Grid>
-          <Grid item mr={2} lg={4} md={5} sm={6} xs={6}>
-             < Heading>{character.name}</Heading>
-             <Text>Actor:{character.actor}</Text>
-             <Text>House:{character.house}</Text>
-             <Text>Father:{character.father}</Text>
-              <Text>Titles:{character.titles.map((title)=>{
-                if(title.includes("Baratheon") || title.includes("Stark") || title.includes("Targaryen"))
-                return;
-                else
-                return <span>{title}<br/></span>;
-              })}</Text>
+          <Grid
+            container
+            sx={styles}
+            lg={8}
+            md={8}
+            sm={8}
+            xs={8}
+            justifyContent="center"
+          >
+            <Grid item lg={4} md={4} sm={4} xs={4}>
+              <CardMedia
+                component="img"
+                alt={character.name}
+                image={character.image}
+              />
+            </Grid>
+            <Grid item mr={1} lg={4} md={4} sm={4} xs={4}>
+              <CardContent>
+                <Heading>{character.name}</Heading>
+                <Text>Actor:{character.actor}</Text>
+                <Text>House:{character.house}</Text>
+                <Text>Father:{character.father}</Text>
+                <Text>
+                  Titles:
+                  {character.titles.map((title) => {
+                    if (
+                      title.includes("Baratheon") ||
+                      title.includes("Stark") ||
+                      title.includes("Targaryen")
+                    )
+                      return;
+                    else
+                      return (
+                        <span>
+                          {title}
+                          <br />
+                        </span>
+                      );
+                  })}
+                </Text>
+              </CardContent>
+            </Grid>
+            <Grid item mr={1} lg={3} md={3} sm={3} xs={3}>
+              <CardContent>
+                <Text>
+                  Origin:
+                  {character.origin.map((ori) => {
+                    return (
+                      <span>
+                        {ori}
+                        <br />
+                      </span>
+                    );
+                  })}
+                </Text>
+                <Text>
+                  Culture:
+                  {character.culture.map((cul) => {
+                    return (
+                      <span>
+                        {cul}
+                        <br />
+                      </span>
+                    );
+                  })}
+                </Text>
+                <Text>
+                  Religion:
+                  {character.religion.map((rel) => {
+                    return (
+                      <span>
+                        {rel}
+                        <br />
+                      </span>
+                    );
+                  })}
+                </Text>
+                <Text>
+                  Spouse:
+                  {character.spouse.map((spous) => {
+                    return (
+                      <span>
+                        {spous}
+                        <br />
+                      </span>
+                    );
+                  })}
+                </Text>
+                <Text>
+                  Lovers:
+                  {character.lovers.map((lover) => {
+                    return (
+                      <span>
+                        {lover}
+                        <br />
+                      </span>
+                    );
+                  })}
+                </Text>
+                <Text>
+                  Siblings:
+                  {character.siblings.map((sibling) => {
+                    return (
+                      <span>
+                        {sibling}
+                        <br />
+                      </span>
+                    );
+                  })}
+                </Text>
+              </CardContent>
+            </Grid>
           </Grid>
-          <Grid item mr={2} lg={3} md={5} sm={6} xs={6}>
-          <Text>Origin:{character.origin.map((ori)=>{
-                return <span>{ori}<br/></span>;
-              })}</Text>
-              <Text>Culture:{character.culture.map((cul)=>{
-                return <span>{cul}<br/></span>;
-              })}</Text>
-              <Text>Religion:{character.religion.map((rel)=>{
-                return <span>{rel}<br/></span>;
-              })}</Text>
-          <Text>Spouse:{character.spouse.map((spous)=>{
-                return <span>{spous}<br/></span>;
-              })}</Text>
-              <Text>Lovers:{character.lovers.map((lover)=>{
-                return <span>{lover}<br/></span>;
-              })}</Text>
-              <Text>Siblings:{character.siblings.map((lover)=>{
-                return <span>{lover}<br/></span>;
-              })}</Text>
-          </Grid>
-        </Grid>
         </Fade>
       </Modal>
     </>
